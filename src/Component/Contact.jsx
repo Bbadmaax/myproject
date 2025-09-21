d038232602e56714eb91b1f6de18b992f407ba1f
Bbadmaax <mustafjourney2025@gmail.com>
2025-09-21 14:54:47 +0300

    add Contant.jsx

---
+++ src/Component/Contact.jsx
@@ -0,0 +1,12 @@
+
+function Contact(){
+
+
+    return(
+        <div>
+            <h1>contact page</h1>
+            <a> contact me this phone: 0907727710</a>
+        </div>
+    )
+}
+export default Contact;
